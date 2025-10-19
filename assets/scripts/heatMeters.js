d7d18938af7a6bb73db17d35d146966b2e2ff633
Dargom <Shoot-nik4@yandex.ru>
2025-10-19 13:53:53 +0300

    завершил heatMeters.html со стилями и скриптами

--- assets/scripts/heatMeters.js
+++ assets/scripts/heatMeters.js
@@ -1,3 +1,52 @@
+/*—————————————————————————————————————————————————————————————————nodes-carousel-section———————————————————————————————————————————————————————*/
+
+let nodesCarousel = document.querySelector(".nodes-carousel");
+let nodesCarouselInner = document.querySelector(".nodes-carousel__inner");
+let nodesCarouselLeftButton = document.querySelector(".nodes-carousel_left-button");
+let nodesCarouselRightButton = document.querySelector(".nodes-carousel_right-button");
+let nodesCarouselItem = Array.from(document.getElementsByClassName("nodes-carousel-item"))[1];
+let slideIndex = 0;
+
+// nodesCarouselLeftButton.addEventListener("click", ()=> {
+//     slideIndex--;
+//     let itemWidth = parseInt(window.getComputedStyle(nodesCarouselItem).getPropertyValue("width"))
+//     let itemMargin = parseInt(window.getComputedStyle(nodesCarouselItem).getPropertyValue("margin-left"))
+//     nodesCarouselInner.style["transform"] = `translateX(${-(itemWidth + itemMargin)*slideIndex}px)`;
+//
+//     console.log(`${-(itemWidth + itemMargin)*slideIndex}`);
+//     console.log(carouselButtonLeft.disabled);
+//     carouselButtonLeft.disabled = slideIndex === 0;
+//     console.log(carouselButtonLeft.disabled);
+//     carouselButtonRight.disabled = slideIndex === itemsAmount;
+// })
+
+nodesCarouselLeftButton.addEventListener("click", ()=> {
+    slideIndex--;
+    let itemWidth = parseInt(window.getComputedStyle(nodesCarouselItem).getPropertyValue("width"))
+    let itemMargin = parseInt(window.getComputedStyle(nodesCarouselItem).getPropertyValue("margin-left"))
+    // nodesCarouselInner.scrollBy(-(itemWidth + itemMargin), 0);
+    nodesCarouselInner.scrollBy({
+        top: 0,
+        left: -(itemWidth + itemMargin),
+        behavior: "smooth"
+});
+})
+
+nodesCarouselRightButton.addEventListener("click", ()=> {
+    slideIndex++;
+    let itemWidth = parseInt(window.getComputedStyle(nodesCarouselItem).getPropertyValue("width"))
+    let itemMargin = parseInt(window.getComputedStyle(nodesCarouselItem).getPropertyValue("margin-left"))
+    // nodesCarouselInner.scrollBy(-(itemWidth + itemMargin), 0);
+    nodesCarouselInner.scrollBy({
+        top: 0,
+        left: (itemWidth + itemMargin),
+        behavior: "smooth"
+    });
+})
+
+
+/*—————————————————————————————————————————————————————————————————item-page-section———————————————————————————————————————————————————————*/
+
 let items = document.getElementsByClassName("item-page__item");
 let elCounts;
 
